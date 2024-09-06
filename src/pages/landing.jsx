2d41e411db9ed4ed8bf8b8cfcbd80b4bab27850c
neeraj500 <neerajmadake18@gmail.com>
2024-09-06 23:10:02 +0530

    misc change

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -38,7 +38,7 @@ const LandingPage = () => {
           Shorten!
         </Button>
       </form>
-      <img src="./banner.jpg" alt="banner" className="w-full h-full my-11 md:px-11" />
+      <img src={"./banner.jpg"} alt="banner" className="w-full h-full my-11 md:px-11" />
 
       <Accordion type="multiple" collapsible="true" className="w-full md:px-11">
         <AccordionItem value="item-1">
